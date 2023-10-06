935ee727a3d433e98c8d06823d84c387c5757324
秋山翔 <35907066+akiyamasho@users.noreply.github.com>
2023-10-07 03:00:14 +0900

    FEAT: return base64 image string in predict

--- StableDiffusionModel.py
+++ StableDiffusionModel.py
@@ -1,10 +1,15 @@
+import os
+import base64
+import io
+
+from PIL import Image
 from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 
 
 class StableDiffusionModel(object):
     def __init__(self):
-        self.pipeline = StableDiffusionPipeline.from_pretrained(
-            "CompVis/stable-diffusion-v1-4"
+        self.pipeline = StableDiffusionPipeline.from_single_file(
+            os.path.join("artifacts", "model.safetensors")
         )
         self.pipeline.scheduler = EulerDiscreteScheduler.from_config(
             self.pipeline.scheduler.config
@@ -12,6 +17,15 @@ class StableDiffusionModel(object):
         pass
 
     def predict(self, positive_prompt: str, negative_prompt: str):
-        output = self.pipeline.run(positive_prompt, negative_prompt)
+        output_image: Image.Image = self.pipeline.run(
+            positive_prompt, negative_prompt
+        ).images[0]
 
-        return
+        buffer = io.BytesIO()
+        output_image.save(buffer, format="PNG")
+        buffer.seek(0)
+
+        data_uri = base64.b64encode(buffer.read()).decode("ascii")
+        base64_image = f"data:image/png;base64,{data_uri}"
+
+        return base64_image
